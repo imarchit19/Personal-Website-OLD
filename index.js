ec598c9adbb4b8b021ef99f6bf6e0950206472ea
Archit Gupta <guptaarchit023@gmail.com>
2019-08-16 00:26:42 +0530

    Added scroll transition and js

---
+++ index.js
@@ -0,0 +1,9 @@
+/*Scroll transition to anchor*/
+$("a.toscroll").on('click', function (e) {
+    var url = e.target.href;
+    var hash = url.substring(url.indexOf("#") + 1);
+    $('html, body').animate({
+        scrollTop: $('#' + hash).offset().top
+    }, 1100);
+    return false;
+});
